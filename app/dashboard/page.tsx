8a9ef5e09f5653a4f076b3a8d309dc3014397827
Esteban Velasquez <estebanvelasquezcardeno@gmail.com>
2024-01-28 12:34:48 -0500

    refac: update dashboard page to use React Suspense and add skeletons

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,13 @@
-import { fetchLatestInvoices, fetchRevenue } from '@/app/lib/data';
-import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '../ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import {
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from '@/app/ui/skeletons';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 
 export default async function DashboardPage() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -23,8 +24,12 @@ export default async function DashboardPage() {
         /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
