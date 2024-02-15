fc72eb439ed40169eb420fee3ed460b5ef22ea83
Esteban Velasquez <estebanvelasquezcardeno@gmail.com>
2024-02-15 18:15:38 -0500

    chore: update prettier.config.js with import order and additional plugins

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,30 @@
 const styleguide = require('@vercel/style-guide/prettier');
 
+/** @type {import('prettier').Config} */
 module.exports = {
   ...styleguide,
-  plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+  importOrder: [
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^types$',
+    '^@/types/(.*)$',
+    '^@/config/(.*)$',
+    '^@/lib/(.*)$',
+    '^@/hooks/(.*)$',
+    '^@/components/ui/(.*)$',
+    '^@/components/(.*)$',
+    '^@/styles/(.*)$',
+    '^@/app/(.*)$',
+    '^@/.*$',
+    '',
+    '^[./]',
+  ],
+  importOrderTypeScriptVersion: '5.0.0',
+  plugins: [
+    ...styleguide.plugins,
+    'prettier-plugin-tailwindcss',
+    '@ianvs/prettier-plugin-sort-imports',
+  ],
 };
